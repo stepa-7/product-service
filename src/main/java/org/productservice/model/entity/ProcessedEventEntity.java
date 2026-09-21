478533ecdd713a821afe6a94e2f036ea4f33ad09
Chumovitskiy S. <schumovitskiy@mail.ru>
2026-09-21 23:29:04 +0300

    Добавлены миграция, сущность, репозиторий для таблицы обработанных событий

---
+++ src/main/java/org/productservice/model/entity/ProcessedEventEntity.java
@@ -0,0 +1,32 @@
+package org.productservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.productservice.model.event.ProductEventType;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "processed_events")
+public class ProcessedEventEntity {
+    @Id
+    @Column(name = "event_id", updatable = false, nullable = false)
+    private UUID eventId;
+
+    @Column(name = "event_type", length = 50, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProductEventType eventType;
+
+    @Column(name = "processed_at", nullable = false)
+    private Instant processedAt;
+}
---
+++ src/main/java/org/productservice/repository/ProcessedEventRepository.java
@@ -0,0 +1,10 @@
+package org.productservice.repository;
+
+import org.productservice.model.entity.ProcessedEventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEventEntity, UUID> {
+
+}
