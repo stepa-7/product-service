f4e8e458f5f60758d9808ee1aaefa6c5418dc1e2
Chumovitskiy S. <schumovitskiy@mail.ru>
2026-09-25 18:22:17 +0300

    Репозитории разнесены по сервисам

--- src/main/java/org/productservice/service/ProductEventService.java
+++ src/main/java/org/productservice/service/ProductEventService.java
@@ -2,20 +2,15 @@ package org.productservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.productservice.exception.ProductNotFoundException;
 import org.productservice.model.entity.ProcessedEventEntity;
-import org.productservice.model.entity.ProductEntity;
 import org.productservice.model.ProductEvent;
 import org.productservice.model.ProductEventPayload;
 import org.productservice.model.ProductEventType;
 import org.productservice.repository.ProcessedEventRepository;
-import org.productservice.repository.ProductRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.Instant;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -23,7 +18,7 @@ import java.util.UUID;
 @Slf4j
 public class ProductEventService {
     private final ProcessedEventRepository eventRepository;
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
     @Transactional
     public void process(ProductEvent event) {
@@ -59,57 +54,13 @@ public class ProductEventService {
     }
 
     private void handleCreate(ProductEvent event) {
-        UUID productId = event.getProduct().getId();
         ProductEventPayload productEventPayload = event.getProduct();
-
-        if (!productRepository.existsById(productId)) { // TODO: mapper
-            productRepository.save(ProductEntity.builder()
-                    .id(productId)
-                    .name(productEventPayload.getName())
-                    .description(productEventPayload.getDescription())
-                    .category(productEventPayload.getCategory())
-                    .price(productEventPayload.getPrice())
-                    .currency(productEventPayload.getCurrency())
-                    .status(productEventPayload.getStatus())
-                    .version(productEventPayload.getVersion())
-                .build());
-
-            log.info("Товар успешно создан productId={}", productId);
-        } else {
-            log.info("Товар уже создан productId={}", productId);
-            return;
-        }
+        productService.createFromEvent(productEventPayload);
     }
 
     private void handleUpdate(ProductEvent event) {
-        UUID productId = event.getProduct().getId();
         ProductEventPayload productEventPayload = event.getProduct();
-
-        Optional<ProductEntity> productEntityOptional = productRepository.getProductEntityById(productId);
-        if (productEntityOptional.isPresent()) {
-            ProductEntity product = productEntityOptional.get();
-
-            Long productVersion = product.getVersion() != null ? product.getVersion() : 0L;
-            Long eventVersion = productEventPayload.getVersion();
-
-            if (eventVersion == null || eventVersion <= productVersion) {
-                log.info("Событие устаревшее eventVersion={} productVersion={}", eventVersion, productVersion);
-                return;
-            }
-            product.setName(productEventPayload.getName()); // TODO: mapper
-            product.setDescription(productEventPayload.getDescription());
-            product.setCategory(productEventPayload.getCategory());
-            product.setPrice(productEventPayload.getPrice());
-            product.setCurrency(productEventPayload.getCurrency());
-            product.setStatus(productEventPayload.getStatus());
-            product.setUpdatedAt(Instant.now());
-
-            productRepository.save(product);
-            log.info("Товар успешно обновлен productId={}", productId);
-        } else {
-            log.info("Товар не существует, для того, чтобы его обновить productId={}", productId);
-            throw new ProductNotFoundException(productId);
-        }
+        productService.updateFromEvent(productEventPayload);
     }
 
     private void validateEvent(ProductEvent event) {
